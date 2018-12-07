b91bb14a6c5308c75bdfe66d38b799a39ef6f674
Ziadelmsre <45654099+Ziadelmsre@users.noreply.github.com>
2018-12-07 13:10:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,12 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("505125121896480780")
+let channel =     client.channels.get("519966478829879309")
 setInterval(function() {
 channel.send(`youtube`);
 }, 25)
 })
  
  
-client.login('NTAzMjIyMzg4MzYwNzQwODc0.DunaMw.j660S1qJj10Vp7LRpxNK_IWcF5Y');
+client.login("NTAzMjIyMzg4MzYwNzQwODc0.Duqv9w.8JyRY9HkowQQ8cFCaOoKLX_mtA0");
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
